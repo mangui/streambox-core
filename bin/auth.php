5984cdacb957c49620fef4d3de7168ef494c1ef0
mangui <mangui@streambox.fr>
2012-06-05 22:29:16 +0800

    log user ip/hostname avoid systematic identification failed log on first HTTP GET without login/password

--- bin/auth.php
+++ bin/auth.php
@@ -4,6 +4,9 @@ global $username;
 
 session_start();
 
+$remote_ip = $_SERVER['REMOTE_ADDR'];
+$remote_host= gethostbyaddr($remote_ip);
+
 list($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']) = explode(':' , base64_decode(substr($_SERVER['HTTP_AUTHORIZATION'], 6)));
 $username=$_SERVER['PHP_AUTH_USER'];
 
@@ -17,21 +20,27 @@ if ($_SESSION['authorized'] == false)
 		if($_SERVER['PHP_AUTH_PW'] == sqlgetuserinfo("password", $_SERVER['PHP_AUTH_USER']))
 		{
 			$_SESSION['authorized'] = true;
-			addlog("AUTH: user [" .$username ."] successfully identified!");
-			addstreaminglog("iStreamDev user [" .$username ."] successfully identified!");
+			addlog("user [" .$username ."] successfully identified! IP/HOST=[" .$remote_ip ."/" .$remote_host ."]");
+			addstreaminglog("user [" .$username ."] successfully identified! IP/HOST=[" .$remote_ip ."/" .$remote_host ."]");
+		}
+
+
+		// login
+		if (!$_SESSION['authorized'])
+		{
+			addlog("Identification failed: " .$_SERVER['PHP_AUTH_USER'] ."/" .$_SERVER['PHP_AUTH_PW'] ."IP/HOST=[" .$remote_ip ."/" .$remote_host ."]");
+			addstreaminglog("Identification failed: " .$_SERVER['PHP_AUTH_USER'] ."/" .$_SERVER['PHP_AUTH_PW'] ."IP/HOST=[" .$remote_ip ."/" .$remote_host ."]");
+
+			header('WWW-Authenticate: Basic Realm="Login please"');
+			header('HTTP/1.0 401 Unauthorized');
+			echo "Incorrect user/password";
+			exit;
 		}
 	}
-
-	// login
-	if (!$_SESSION['authorized'])
+	else
 	{
-		addlog("AUTH: identification failed: " .$_SERVER['PHP_AUTH_USER'] ."/" .$_SERVER['PHP_AUTH_PW']);
-		addstreaminglog("iStreamDev identification failed: " .$_SERVER['PHP_AUTH_USER'] ."/" .$_SERVER['PHP_AUTH_PW']);
-
 		header('WWW-Authenticate: Basic Realm="Login please"');
 		header('HTTP/1.0 401 Unauthorized');
-		echo "Incorrect user/password";
-		exit;
 	}
 }
 
